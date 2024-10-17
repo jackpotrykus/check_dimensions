8e8f8a28108e164e64361b0f7aefc9467e33e14a
Jack Potrykus <jackpotrykus@gmail.com>
2024-10-16 21:54:09 -0400

    initial commit 2

--- check_dimension.py
+++ check_dimension.py
@@ -31,6 +31,7 @@ def check_dimensions_against_spec(
     for kwarg, dimensions in dimension_by_kwarg.items():
         for axis_dimension, sym in zip(dimensions, dimension_spec_by_kwarg[kwarg].split(",")):
             sym = sym.strip()
+            # Handle the case that axis dimension is specified exactly
             try:
                 int_sim = int(sym)
                 if int_sim != axis_dimension:
@@ -38,6 +39,7 @@ def check_dimensions_against_spec(
                         f"Expected dimension {sym} in {kwarg} to have dimension {int_sim} but got {axis_dimension}"
                     )
             except ValueError:
+                # Handles the case that the axis dimension is a symbol
                 if value_by_sym[sym] is None:
                     value_by_sym[sym] = axis_dimension
                 elif value_by_sym[sym] != axis_dimension:
@@ -46,22 +48,28 @@ def check_dimensions_against_spec(
                     )
 
     # TODO: obviously delete...
-    print("Passed the dimension check")
-    print("Here are the dimensions by symbol")
     print(value_by_sym)
 
 
-def check_dimensions(**dimension_spec_by_kwarg: dict[str, str]) -> Callable[P, T]:
-    print(get_unique_syms_from_dictionary_of_specs(dimension_spec_by_kwarg))
+def validate_dimension_spec_against_arguments(dimension_spec_by_kwarg: dict[str, str], value_by_kwarg: dict[str, Any]) -> None:
+    for kwarg in dimension_spec_by_kwarg:
+        if kwarg not in value_by_kwarg:
+            raise KeyError(f"Expected keyword argument {kwarg} but it was not provided")
 
+
+def check_dimensions(**dimension_spec_by_kwarg: dict[str, str]) -> Callable[P, T]:
+    # TODO: Obviously delete...
+    print(get_unique_syms_from_dictionary_of_specs(dimension_spec_by_kwarg))
     def decorator(f: Callable[P, T]) -> Callable[P, T]:
         def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
             value_by_kwarg = create_dictionary_of_values_by_argument(f, *args, **kwargs)
-            array_by_kwarg = {k: v for k, v in value_by_kwarg.items() if isinstance(v, np.ndarray)}
-            dimension_by_kwarg = {k: tuple(int(i) for i in v.shape) for k, v in array_by_kwarg.items()}
+            # Validate the supplied dimension spec against the supplied arguments
+            validate_dimension_spec_against_arguments(dimension_spec_by_kwarg, value_by_kwarg)
+
+            arraylike_by_kwarg = {k: value_by_kwarg[k] for k in dimension_spec_by_kwarg}
+            dimension_by_kwarg = {k: tuple(int(i) for i in v.shape) for k, v in arraylike_by_kwarg.items()}
             check_dimensions_against_spec(dimension_spec_by_kwarg, dimension_by_kwarg)
+
             return f(*args, **kwargs)
-
         return wrapper
-
     return decorator
